93607060b0de56c032553900b37da05efadf194f
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2017-08-30 20:32:23 +0300

    Get the stations list

--- app/src/main/java/com/leppaaho/oskari/citybikewidget/ConfigurationActivity.java
+++ app/src/main/java/com/leppaaho/oskari/citybikewidget/ConfigurationActivity.java
@@ -5,9 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
@@ -26,7 +43,50 @@ public class ConfigurationActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        requestWithSomeHttpHeaders();
     }
+
+        public void requestWithSomeHttpHeaders() {
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
+            JSONObject data = null;
+
+            try {
+                data = new JSONObject(
+                        "{\"query\": \"{ bikeRentalStations { name stationId } }\" }"
+                );
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Log.i("INFO", data.toString());
+
+            JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, data,
+                    new Response.Listener<JSONObject>()
+                    {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // response
+                            Log.d("Response", response.toString());
+
+                            final TextView mTxtDisplay;
+                            mTxtDisplay = (TextView) findViewById(R.id.station_list);
+                            mTxtDisplay.setText(response.toString());
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO Auto-generated method stub
+                            Log.d("ERROR","error => "+error.toString());
+                        }
+                    }
+            );
+            queue.add(postRequest);
+
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
