c4d0fde233937e45952b82d4b245d002901848c8
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2017-09-12 19:08:56 +0300

    Better comment wording

--- app/src/main/java/com/leppaaho/oskari/citybikewidget/ConfigurationActivity.java
+++ app/src/main/java/com/leppaaho/oskari/citybikewidget/ConfigurationActivity.java
@@ -76,7 +76,7 @@ public class ConfigurationActivity extends AppCompatActivity {
                 intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
 
                 // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
-                // since it seems the onUpdate() is only fired on that.
+                // since it seems the onUpdate() is only fired like this.
                 int[] ids = {appWidgetId};
 
                 intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
